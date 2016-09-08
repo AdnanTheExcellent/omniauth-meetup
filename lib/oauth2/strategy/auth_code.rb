320d0509d00eaa84eb6803e7065c141490a50a73
Adnan Abdulally <adnan@mgxcopy.com>
2016-09-07 19:54:25 -0700

    Update auth_code.rb
    update class to get working with rails 5. I had to import the rest of the code from the omniauth gem and update the params hash to be a symbol instead of a string.

--- lib/oauth2/strategy/auth_code.rb
+++ lib/oauth2/strategy/auth_code.rb
@@ -9,8 +9,16 @@ require 'oauth2/strategy/base'
 module OAuth2
   module Strategy
     class AuthCode  < Base
+      def authorize_params(params = {})
+        params.merge('response_type' => 'code', 'client_id' => @client.id)
+      end
+
+      def authorize_url(params = {})
+        @client.authorize_url(authorize_params.merge(params))
+      end
+
       def get_token(code, params = {}, opts = {})
-        params['redirect_uri'] = params['redirect_uri'].split('?').first if params.has_key? 'redirect_uri'
+        params[:redirect_uri] = params[:redirect_uri].split('?').first if params.has_key? :redirect_uri
         params = {
             'grant_type' => 'authorization_code',
             'code' => code
